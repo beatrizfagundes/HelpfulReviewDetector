e041fe92ed24cd25dcef87b6318355fbff4aa180
Beatriz Fagundes <beatrizsfslima@gmail.com>
2019-04-28 11:35:42 -0300

    Allow number of splits as argument to split_dataset

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -8,16 +8,18 @@ def main():
     logging.basicConfig(format='%(asctime)s - %(message)s',
                         datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO,
                         filename='split_dataset.log')
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         logging.info('ERROR! Wrong number of params, you must follow the \
 usage:')
-        logging.info("python split_dataset.py [csv file]")
+        logging.info("python split_dataset.py [csv file] [num of splits]")
         return
     dataset = sys.argv[1]
     logging.info('with dataset: %s' % dataset)
+    n = int(sys.argv[2])
+    logging.info('into %s parts' % str(n))
     df = pd.read_csv(dataset)
     count = 1
-    for chunk in np.array_split(df, 8):
+    for chunk in np.array_split(df, n):
         logging.info(chunk.shape)
         chunk.to_csv(dataset.replace('.csv', str(count)+'.csv'), header=True, index=None)
         count += 1
